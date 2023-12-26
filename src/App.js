46863eb88545b92449713de2541513beede0ed6f
brayanperutty <brayanperutty@gmail.com>
2023-12-26 10:07:54 -0500

    Se finaliza el proyecto de Clone de la sección de testimonios de freeCodeCamp

--- src/App.js
+++ src/App.js
@@ -6,7 +6,28 @@ function App() {
     <div className="App">
       <div className='contenedor-principal'>
       <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-      <Testimonio />
+      <Testimonio 
+        name='Brayan Perutty'
+        country='Colombia'
+        imagen='yo.jpg'
+        job='Estudiante de ingeniería de sistemas'
+        company='UFPS'
+        testimonio='Gracias a React he podido crecer profesionalmente. Las oportunidades laborales han aumentado significativamente. Animo a los nuevos estudiantes a que exploren
+                    el mundo que React nos ofrece.' />
+      <Testimonio 
+        name='Thanos Alfonso'
+        country='Planeta Thanos'
+        imagen='thanos.png'
+        job='Villano de todas las personas'
+        company='el mundo'
+        testimonio='Yo soy inevitable. Todos aquellos que han querido enfrentarme han fracasado en el intento. Con el poder de las gemas dominaré el mundo.' />
+       <Testimonio 
+        name='Sr. Stark'
+        country='USA'
+        imagen='stark.jpg'
+        job='Iron Man'
+        company='el mundo'
+        testimonio='La verdad es que soy el mejor de los avengers. Puedo salvar al mundo sin necesidad de los demás por la alta tecnología que poseo. También soy multimillonario.' />
       </div>
     </div>
   );
--- src/components/Testimonio.js
+++ src/components/Testimonio.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import '../stylesheets/Testimonio.css';
 
-function Testimonio(){
+function Testimonio( props ){
   return(
     <div className='contenedor-testimonio'>
       <img 
         className='imagen-testimonio' 
-        src={require('../img/yo.jpg')}
-        alt='Foto de Brayan'/>
+        src={require(`../img/${props.imagen}`)}
+        alt='Foto de Brayan' />
         <div className='contenedor-texto-testimonio'>
-          <p className='nombre-testimonio'>Brayan Perutty en Colombia</p>
-          <p className='cargo-testimonio'>Estudiante ingeniería de sistemas UFPS</p>
-          <p className='texto-testimonio'>"Aprender React ha sido la mejor decisión que he podido tomar. Las oportunidades laborales han aumentado considerablemente. Gracias a React he podido escalar profesionalmente"</p>
+          <p className='nombre-testimonio'>
+            <strong>{props.name}</strong> en <strong>{props.country}</strong>
+          </p>
+          <p className='cargo-testimonio'>
+            {props.job} en <strong>{props.company}</strong>
+          </p>
+          <p className='texto-testimonio'>
+            "{props.testimonio}"
+          </p>
         </div>
     </div>
   );
